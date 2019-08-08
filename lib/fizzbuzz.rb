7da50a36ea6464e0e630a4326c2bdd222ec27904
Mark Dobbins <mjdobbins12@gmail.com>
2019-08-08 11:51:27 +0000

    Add final tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
-def fizzbuzz(number)
-	'fizz'
+class Integer
+def fizzbuzz
+  if self % 3 == 0 && self % 5 != 0
+	return "fizz"
+  elsif self % 5 == 0 && self % 3 != 0
+	return "buzz"
+  elsif self % 15 == 0
+    return "fizzbuzz"
+  else 
+  	return self
+  end
+end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,30 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
 	it 'returns "fizz" when passed 3' do
-		expect(fizzbuzz(3)).to eq 'fizz'
+		expect(3.fizzbuzz).to eq 'fizz'
+	end
+	
+	it 'returns "buzz" when passed 5' do
+		expect(5.fizzbuzz).to eq 'buzz'
+	end
+	
+	it 'returns "fizzbuzz" when passed 15' do
+		expect(15.fizzbuzz).to eq 'fizzbuzz'
+	end
+	
+	it 'returns "fizz" when passed 6' do
+		expect(6.fizzbuzz).to eq 'fizz'
+	end
+	
+	it 'returns 7 when passed 7' do
+		expect(7.fizzbuzz).to eq 7
+	end
+	
+	it 'returns "buzz" when passed 20' do
+		expect(20.fizzbuzz).to eq 'buzz'
+	end 
+	
+	it 'returns "fizzbuzz" when passed 30' do	
+		expect(30.fizzbuzz).to eq 'fizzbuzz'
 	end
 end
